3e527c350649a7a2edba001015c192148dc30332
sayap <devnull@localhost>
2013-01-04 08:56:35 +0800

    Add setup.py for packaging.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='pyaes',
+    version='1.0',
+    author='Marti Raudsepp',
+    author_email='marti@juffo.org',
+    description='Simple AES cipher implementation in pure Python',
+    license='MIT',
+    keywords='aes pypy crypto',
+    url='https://bitbucket.org/intgr/pyaes',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Security :: Cryptography',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    py_modules=['pyaes'],
+)
